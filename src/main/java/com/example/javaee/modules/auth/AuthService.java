b0c11b043d2ef2b0103299b95e795f9ebb648b74
init-armat <armat@init.red>
2024-12-06 08:51:06 +0500

    feat: email

--- src/main/java/com/example/javaee/modules/auth/AuthService.java
+++ src/main/java/com/example/javaee/modules/auth/AuthService.java
@@ -1,6 +1,7 @@
 package com.example.javaee.modules.auth;
 
 import com.example.javaee.entities.User;
+import com.example.javaee.modules.email.EmailService;
 import com.example.javaee.modules.user.UserService;
 import com.example.javaee.modules.user.dto.CreateUserDto;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,10 +11,12 @@ import org.springframework.stereotype.Service;
 public class AuthService {
   private final UserService userService;
   private final PasswordEncoder passwordEncoder;
+  private final EmailService emailService;
 
-  public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
+  public AuthService(UserService userService, PasswordEncoder passwordEncoder, EmailService emailService) {
     this.userService = userService;
     this.passwordEncoder = passwordEncoder;
+    this.emailService = emailService;
   }
 
   public User registerUser(CreateUserDto createUserDto) {
@@ -23,6 +26,8 @@ public class AuthService {
     newUser.setUsername(createUserDto.getUsername());
     newUser.setEmail(createUserDto.getEmail());
     newUser.setPassword(hashedPassword);
+
+    emailService.sendGreetingEmail(createUserDto.getEmail(), "Welcome to my site!", "Thank you for registration! It is great to see you on my site.");
 
     return userService.saveUser(newUser);
   }
